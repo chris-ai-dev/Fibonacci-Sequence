ef814f1659a33294cb12c5d36d9bef726ec93f92
Chris <chrisvolar@gmail.com>
2022-05-24 15:59:07 -0500

    fibjs code

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,23 @@
 const fibonacci = (num) => {
-    
+
+    //edge case
+    if(num ===0 || num ===1) {
+        return console.log(num);
+    }
+   //to get each new fib number you add the two in front of it
+   
+   let numberOne = 0;
+   let numberTwo = 1;
+   let sum = numberOne + numberTwo;
+   //create a for loop to add each number till reach out final result
+   for(let i = 2; i < num; i++) {
+        numberOne = numberTwo;
+        numberTwo = sum;
+        sum = numberOne + numberTwo;
+   }
+   
+   return console.log(sum);
 }
 
-module.exports = {fibonacci}
+fibonacci(10) //-> 2
+//module.exports = {fibonacci}
