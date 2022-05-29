799b88d14febcb6e6a975401c13f73bc60494f70
Chris <chrisvolar@gmail.com>
2022-05-28 20:32:40 -0500

    yay python

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,9 @@
 def fibonacci(n):
-  pass
-
-
+  if n == 1 or n == 0:
+    return n
+  fib_list = [0,1]
+  for i in range(n-1):
+    fib_list.append(fib_list[i] + fib_list[i+1])
+  return fib_list[n]
+    
+#print(fibonacci(11))
